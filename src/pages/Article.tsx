d4d079c6baad01886d7e864c28db7e789f836343
Vishal Mehta <vvmehta06@gmail.com>
2026-08-09 11:34:01 -0400

    ADD: Error Pages

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,8 +1,9 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { articles } from "../articles";
 
 function Article() {
     const { course, module, article } = useParams();
+    const navigate = useNavigate();
 
     if (!course || !module || !article) {
         return <h1>Article not found</h1>;
@@ -14,7 +15,14 @@ function Article() {
         articles[articleKey as keyof typeof articles];
 
     if (!ArticleContent) {
-        return <h1>Article not found</h1>;
+        return (
+        <div className="min-h-screen content-center text-center">
+            <h1 className="text-3xl text-error font-bold">Article not found</h1>
+            <button type="button" onClick={() => navigate(-1)} className="py-3 px-5 my-10 rounded-xl cursor-pointer bg-panel-2 text-text-2">
+                Go Back
+            </button>
+        </div>
+        );
     }
 
     return (
--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,16 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import CourseContent from "../components/CourseContent";
 import { courses } from "../courses";
 
 function Course() {
     const { course } = useParams();
+    const navigate = useNavigate();
 
     const selectedCourse = course
         ? courses[course as keyof typeof courses]
         : undefined;
 
     if (!selectedCourse) {
-        return <h1>Course not found</h1>;
+        return (
+        <div className="min-h-screen content-center text-center">
+            <h1 className="text-3xl text-error font-bold">Course not found</h1>
+            <button type="button" onClick={() => navigate(-1)} className="py-3 px-5 my-10 rounded-xl cursor-pointer bg-panel-2 text-text-2">
+                Go Back
+            </button>
+        </div>
+        );
     }
 
     return (
